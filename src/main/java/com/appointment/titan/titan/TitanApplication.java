298bc60cd8a66f0139e687186b27892f5a6157e9
Gaurav Oli <gauravoli85@gmail.com>
2018-06-03 14:31:30 -0400

    initial commit

---
+++ src/main/java/com/appointment/titan/titan/TitanApplication.java
@@ -0,0 +1,12 @@
+package com.appointment.titan.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanApplication.class, args);
+	}
+}
